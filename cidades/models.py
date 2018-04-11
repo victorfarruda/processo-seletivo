abb1c801dd400c2bbd05a78722e4982282150ade
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-04-11 17:19:59 -0300

    model cidade

--- cidades/models.py
+++ cidades/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from cursos.models import Curso
+
+class Cidade(models.Model):
+    cidade = models.CharField(max_length=120)
+    curso  = models.ManyToManyField(Curso)
+
+    def __str__(self):
+        return self.cidade
