80d6b0b32df8abca5a0c88e18601353635ef949b
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-04-11 18:02:11 -0300

    models usuario e perfil

--- contas/models.py
+++ contas/models.py
@@ -1,28 +1,67 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
 from enderecos.models import Endereco
 
-# class Perfil(models.Model):
-#     nome = models.CharField(max_length=120)
-#     sobrenome = models.CharField(max_length=120)
-#     data_nasc = models.DateField()
-#     telefone = models.CharField(max_length=20)
-#     rg        = models.CharField(max_length=20, unique=True)
-#     cpf       = models.CharField(max_length=15, unique=True)
-#     timestamp = models.DateTimeField(auto_now_add=True)
-#     updated   = models.DateTimeField(auto_now=True)
-#
-#     def __str__(self):
-#         return self.nome
+class Perfil(models.Model):
+    nome = models.CharField(max_length=120)
+    sobrenome = models.CharField(max_length=120)
+    data_nasc = models.DateField()
+    telefone = models.CharField(max_length=20)
+    rg        = models.CharField(max_length=20, unique=True)
+    cpf       = models.CharField(max_length=15, unique=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated   = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Usuário deve ter endereço de e-mail')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+        user.is_staff = True
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 class Usuario(AbstractBaseUser):
-    email = models.CharField(max_length=120, unique=True)
-    # perfil = models.OneToOneField(Perfil)
-    endereco = models.OneToOneField(Endereco)
+    email     = models.CharField(max_length=120, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_admin  = models.BooleanField(default=False)
+    is_staff  = models.BooleanField(default=False)
+    perfil    = models.OneToOneField(Perfil, blank=True, null=True)
+    endereco  = models.OneToOneField(Endereco, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
+
+    objects = MyUserManager()
 
     def __str__(self):
         return self.perfil
 
+    def get_short_name(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
