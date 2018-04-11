d95ed9ecc4a4a2448c6b01296f20e35c151f3aa2
Victor de Freitas Arruda <victorf_arruda@outlook.com>
2018-04-11 17:15:15 -0300

    model curso

--- cursos/models.py
+++ cursos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Curso(models.Model):
+    curso = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.curso
